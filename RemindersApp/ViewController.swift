0239369299ed9ca59fe7575fb2b11c78f42c588f
alperenkavukk <kavuk.67.76@gmail.com>
2022-10-13 22:15:41 +0300

    Initial Commit

---
+++ RemindersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemindersApp
+//
+//  Created by Alperen Kavuk on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
